18211d8089822dbcf7ebe5ef1dbfca2d075fbe74
Henrique Martins <hmartins.dev@gmail.com>
2025-07-13 22:25:48 -0300

    fix: add JS support files in eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,16 +1,16 @@
 import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx}', '**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
